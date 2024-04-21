675ad09c5fa38a1b730054a88992fc3b52085dc0
zepri-lee <zepri123@gmail.com>
2024-04-21 12:28:44 +0900

    간단수정

--- src/main/java/com/psc/example/q102/Q102Application.java
+++ src/main/java/com/psc/example/q102/Q102Application.java
@@ -1,17 +1,18 @@
 package com.psc.example.q102;
 
 import java.util.UUID;
-
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -22,37 +23,45 @@ import org.springframework.transaction.PlatformTransactionManager;
 @SpringBootApplication
 public class Q102Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Q102Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Q102Application.class, args);
+  }
 
-	@Bean
-	ApplicationRunner runner(JobLauncher jobLauncher, Job job) {
-		return args -> {
-			var jobParameters = new JobParametersBuilder().addString("uuid", UUID.randomUUID().toString())
-					.toJobParameters();
-			var run = jobLauncher.run(job, jobParameters);
-			var instanceId = run.getJobInstance().getInstanceId();
-			System.out.println("InstanceId : " + instanceId);
-		};
+  @Bean
+  Job job(JobRepository jobRepository, Step step) {
+    return new JobBuilder("job", jobRepository)
+        .start(step)
+        .build();
+  }
+  
+  @Bean
+  ApplicationRunner runner(JobLauncher jobLauncher, Job job) {
+    return args -> {
+      var jobParameters = new JobParametersBuilder()
+          .addString("uuid", UUID.randomUUID()
+                            .toString())
+                            .toJobParameters();
+      var run = jobLauncher.run(job, jobParameters);
+      var instanceId = run.getJobInstance().getInstanceId();
+      System.out.println("InstanceId : " + instanceId);
+    };
 
-	}
+  }
+  
+  @Bean
+  @StepScope
+  Tasklet tasklet(@Value("#{jobParameters['uuid']}") String uuid) {
+    return (contribution, context) -> {
+      System.out.println("Hello World! - " + uuid);
+      return RepeatStatus.FINISHED;
+    };
+  }  
 
-	@Bean
-	Tasklet tasklet() {
-		return (contribution, context) -> {
-			System.out.println("Hello World!");
-			return RepeatStatus.FINISHED;
-		};
-	}
-
-	@Bean
-	Job job(JobRepository jobRepository, Step step) {
-		return new JobBuilder("job", jobRepository).start(step).build();
-	}
-
-	@Bean
-	Step step1(JobRepository jobRepository, Tasklet tasklet, PlatformTransactionManager transactionManager) {
-		return new StepBuilder("step1", jobRepository).tasklet(tasklet, transactionManager).build();
-	}
+  @Bean
+  Step step1(JobRepository jobRepository, Tasklet tasklet,
+      PlatformTransactionManager transactionManager) {
+    return new StepBuilder("step1", jobRepository)
+        .tasklet(tasklet, transactionManager)
+        .build();
+  }
 }
